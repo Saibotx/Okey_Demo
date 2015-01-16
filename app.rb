84797ca52a52b9e31d13d3a8846072d580f11d63
unknown <tobias.goo@gmail.com>
2015-01-15 20:35:02 -0500

    Initial commit

---
+++ app.rb
@@ -0,0 +1,61 @@
+require 'rubygems'
+require 'sinatra'
+
+get '/hi' do
+	'dsfsdf'
+end
+
+get '/approved' do
+	File.open('approved-id.log', 'a') do |f|
+		f.write(params['id'] + "\n")
+	end
+end
+
+get '/find' do
+	id = params['id']
+	
+	registered_ids = []
+	
+	File.open('approved-id.log', 'r').each_line do |l|
+		registered_ids << l.sub("\n", "")
+	end
+	
+	ret = "false"
+	
+	registered_ids.each do |rid|
+		if rid == id
+			ret = "true"
+		end
+	end
+	
+	ret
+end
+
+get '/denied' do
+	id = params['id']
+	
+	registered_ids = []
+	
+	File.open('approved-id.log', 'r').each_line do |l|
+		registered_ids << l.sub("\n", "")
+	end
+	
+	ret = "false"
+	
+	registered_ids.each do |rid|
+		if rid == id
+			registered_ids.delete(rid)
+		end
+	end
+	
+	File.open('approved-id.log', 'w') do |f|
+		f.write(registered_ids.join("\n"))
+		f.write("\n")
+	end
+end
+
+get '/test' do
+	'hi toby'
+	
+	erb :test
+end
