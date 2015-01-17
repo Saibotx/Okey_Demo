1eb0dc29ef30aa89bbe90928c82fbc51cca87b28
unknown <tobias.goo@gmail.com>
2015-01-16 22:49:02 -0500

    finished lock

--- app.rb
+++ app.rb
@@ -7,11 +7,13 @@ end
 
 
 get '/Lock' do
+	device = "lock"
 	id = params['id']
 	approve = params['approve']
 	deny = params['deny']
 	find = params['find']
 	viewlog = params['viewlog']
+	delete = params['delete']
 	
 	if id.nil?
 		"MUST SPECIFY ID"
@@ -22,17 +24,75 @@ get '/Lock' do
 		if deny.nil?
 			if find.nil?
 				if viewlog.nil?
-					"ERROR: Put a command"
+					if delete.nil?
+						"ERROR: Put a command"
+					else
+						#doing delete
+						if File.exist?("./tmp/#{device}-approved-id.log")
+							File.delete("./tmp/#{device}-approved-id.log")
+							"File deleted"
+						else
+							"FILE DOES NOT EXIST - DELeTE"
+						end
+					end
 				else
+					#viewlog stuff goes here
 					"doing viewlog"
 				end
 			else
-				"doing find"
+				#doing find
+				registered_ids = []
+				if File.exist?("./tmp/#{device}-approved-id.log")
+					File.open("./tmp/#{device}-approved-id.log" 'r').each_line do |l|
+						registered_ids << l.sub("\n", "")
+					end
+					ret = "false find"
+					registered_ids.each do |rid|
+						if rid == id
+							ret = "true find"
+						end
+					end
+					ret
+				else
+					ret = "device file not found - Find"
+					ret
+				end
+			end				
+		else
+			#doing deny
+			registered_ids = []
+			if File.exist?("./tmp/#{device}-approved-id.log")
+				File.open("./tmp/#{device}-approved-id.log", 'r').each_line do |l|
+					registered_ids << l.sub("\n", "")
+				end
+				ret = "false"
+				registered_ids.each do |rid|
+					if rid == id
+						registered_ids.delete(rid)
+					end
+				end
+					File.delete("./tmp/#{device}-approved-id.log")
+					myfile = File.new("./tmp/#{device}-approved-id.log", "w+")
+				myfile.puts(registered_ids.join("\n"))
+				myfile.puts("\n")
+				myfile.close
+				"doing DENY"
+			end
+			else
+			"device file not found - DENY"
+			end
+		end	
+	else
+		#do approved
+		if File.exist?("./tmp/#{device}-approved-id.log")
+			File.open("./tmp/#{device}-approved-id.log") do |f|
+				f.write(params['id']+"\n")
 			end
 		else
-			"doing DENY"
+			myfile = File.new("./tmp/#{device}-approved-id.log", "w+")
+			myfile.puts(params['id'] + "\n")
+			myfile.close
 		end
-	else
 		"DOING APPROVE"
 	end
 		
